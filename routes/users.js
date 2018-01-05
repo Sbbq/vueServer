af5b9db6b65d55c68ab8a9cd925685a6677e27b9
zkLike <yxzzkx@163.com>
2018-01-05 15:07:16 +0800

    update

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,4 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/a', function(req, res, next) {
-  res.send('drtg');
-});
-
-router.get('/b', function(req, res, next) {
-  res.send('drtg');
-});
-
 module.exports = router;
