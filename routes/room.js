bfd89343021f3256ada4f3115a957fa2cdc167fb
zkLike <yxzzkx@163.com>
2018-01-08 16:58:53 +0800

    add sortflag

--- routes/room.js
+++ routes/room.js
@@ -21,9 +21,9 @@ router.get("/",function(req,res,next){
 	let pageSize=parseInt(req.query.pageSize);
 	let priceUp=parseInt(req.query.priceUp);
 	let priceDown=parseInt(req.query.priceDown);
-	console.log(req.query.sortFlag);
-	console.log(priceDown);
-	console.log(priceUp);
+	let sortFlag;
+	if(req.query.sortFlag=="true")sortFlag=1;
+	if(req.query.sortFlag=="false")sortFlag=-1;
 	let skip=(page-1)*pageSize;
 	let params={
 		roomPrice:{
@@ -31,7 +31,7 @@ router.get("/",function(req,res,next){
 			$lte:priceUp
 		}
 	};
-	Room.find(params).skip(skip).limit(pageSize).sort({roomPrice:req.query.sortFlag}).exec(function(err,result){
+	Room.find(params).skip(skip).limit(pageSize).sort({roomPrice:sortFlag}).exec(function(err,result){
 		if(err){
 			res.json({
 				status:"1",
